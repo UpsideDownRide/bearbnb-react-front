1d570fc9140f13a93709c8dbf6941c452e3e5823
Radomir Nowacki <kenderpl@gmail.com>
2022-07-19 23:57:29 +0200

    user menu dropdown

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -2,11 +2,11 @@ import { Container, Grid, Header } from '@mantine/core';
 import { UserMenu } from 'components/header/UserMenu'
 
 function AppHeader() {
-    const heightLogo = "75px"
+    const heightLogo = "55px"
     return (
         <Header height={80}>
-            <Container size={1440} px={0}>
-                <Grid gutter={0}> 
+            <Container size={1440} style={{height: "100%"}}>
+                <Grid gutter={0} justify="space-between" align="center" style={{height: "100%"}}>
                     <Grid.Col span={4}>
                         <img src="bearbnb.png" alt="Bearbnb logo" height={heightLogo} />
                     </Grid.Col>
@@ -14,8 +14,10 @@ function AppHeader() {
                         <div>Search settings</div>
                     </Grid.Col>
                     <Grid.Col span={4}>
-                        <div>Right side components</div>
-                        <UserMenu/>
+                        <Grid justify="flex-end">
+                            <Grid.Col span={10}><div>Right side components</div></Grid.Col>
+                            <Grid.Col span={2}><UserMenu /></Grid.Col>
+                        </Grid>
                     </Grid.Col>
                 </Grid>
             </Container>
--- src/components/header/UserMenu.tsx
+++ src/components/header/UserMenu.tsx
@@ -1,5 +1,23 @@
+import { Burger, Divider, Menu } from "@mantine/core";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { ROUTES } from "appConstants";
+
 function UserMenu() {
-    return null;
+    const [opened, setOpened] = useState(false);
+    const title = opened ? 'Close navigation' : 'Open Navigation';
+    return (
+        <Menu control={<Burger size="sm" opened={opened}
+                        onClick={() => setOpened(o => !o)}
+                        title={title}/>}
+        >
+            <Menu.Item component={Link} to={ROUTES.SIGNUP}>Register</Menu.Item>
+            <Menu.Item component={Link} to={ROUTES.SIGNIN}>Login</Menu.Item>
+            <Divider/>
+            <Menu.Item>Become host</Menu.Item>
+            <Menu.Item>Help</Menu.Item>
+        </Menu>
+    )
 }
 
 export { UserMenu }
